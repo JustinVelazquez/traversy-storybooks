d1552c411e68ff4f233b94ffa889f64b5f35e4be
Justin V <justinv4994@gmail.com>
2021-04-05 19:01:22 -0400

    Added Google oAuth/ saved OAuth Session

---
+++ README.md
@@ -0,0 +1,12 @@
+Based on The Traversy Media Node.js App video. A tutorial that teaches how to create a full-stack web storybook application.
+
+Technologies Used:
+
+Javascript
+Node
+MongoDB Atlas
+Express
+Handlebars
+Google OAuth
+
+Link: https://www.youtube.com/watch?v=SBvmnHTQIPY
---
+++ index.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+//Load config
+dotenv.config({ path: './config/config.env' });
+
+//passport config
+require('./config/passport')(passport);
+
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Logging
+process.env.NODE_ENV === 'development'
+  ? app.use(morgan('dev'))
+  : app.use(morgan('common'));
+
+//handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+//Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Routes
+app.use('/', require('./routes/routes'));
+app.use('/auth', require('./routes/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  );
+});
